c87abfa1268a374e7bbf78ad510d7e734e7f4fed
rodionovv <wasyarod@mail.ru>
2019-10-08 16:52:09 +0300

    Scripted auto-commit on change (2019-10-08 16:52:09) by gitwatch.sh

--- src/main/java/Lab1.java
+++ src/main/java/Lab1.java
@@ -1,2 +1,67 @@
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+import java.io.IOException;
+import java.util.StringTokenizer;
+
 public class Lab1 {
+    public static class WordMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
+
+        private final static IntWritable one = new IntWritable(1);
+        private Text word = new Text();
+
+        @Override
+        protected void map(LongWritable key, Text value, Context context
+        ) throws IOException, InterruptedException {
+            StringTokenizer itr = new StringTokenizer(value.toString().replaceAll("[^a-zA-Z0-9\\s+]", "").toLowerCase());
+
+            while(itr.hasMoreTokens()){
+                word.set(itr.nextToken());
+                context.write(word, one);
+            }
+
+        }
+    }
+
+    public static class WordReducer extends Reducer<Text, IntWritable, Text, LongWritable> {
+
+        private LongWritable res = new LongWritable();
+
+        @Override
+        protected void reduce(Text key, Iterable<IntWritable> values, Context context) throws
+                IOException, InterruptedException {
+            int sum = 0;
+            for (IntWritable val : values){
+                sum += val.get();
+            }
+            res.set(sum);
+            context.write(key, res);
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        if (args.length != 2) {
+            System.err.println("Usage: WordCountApp <input path> <output path>");
+            System.exit(-1);
+        }
+        Job job = Job.getInstance();
+        job.setJarByClass(WordCountApp.class);
+        job.setJobName("Word count");
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+        job.setMapperClass(WordMapper.class);
+        job.setReducerClass(WordReducer.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+        job.setNumReduceTasks(2);
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
+    }
+
 }
