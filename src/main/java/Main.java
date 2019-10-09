9de4c5e892a40eecbe5a15b86c4d9caa6ea82a5e
rodionovv <wasyarod@mail.ru>
2019-10-09 13:07:39 +0300

    Scripted auto-commit on change (2019-10-09 13:07:39) by gitwatch.sh

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -27,8 +27,12 @@ public class Main {
     public static class SystemJoinMapper extends Mapper<LongWritable, Text, TextPair, Text> {
         @Override
         protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
-            SystemInfo system = new SystemInfo(value);
-            context.write(new TextPair(system.getSystemCode().toString(), "0"), new Text(system.toString()));
+
+            
+
+
+//            SystemInfo system = new SystemInfo(value);
+//            context.write(new TextPair(system.getSystemCode().toString(), "0"), new Text(system.toString()));
         }
     }
 
