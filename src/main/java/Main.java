0ecb76f98081d58798770907d550e56befe262a7
rodionovv <wasyarod@mail.ru>
2019-10-09 15:13:06 +0300

    Scripted auto-commit on change (2019-10-09 15:13:06) by gitwatch.sh

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -108,6 +108,7 @@ public class Main {
                 return;
             }
             String record = value.toString();
+            System.out.println(record);
             String[] parts = record.split(",");
             context.write(new TextPair(parts[0], "0"), new Text(parts[1]));
         }
