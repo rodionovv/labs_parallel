b1c8509958c5dff941e4f87fadb029392c1c1202
rodionovv <wasyarod@mail.ru>
2019-10-09 12:32:40 +0300

    Scripted auto-commit on change (2019-10-09 12:32:40) by gitwatch.sh

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,20 +10,34 @@ import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
-
-
+import java.io.IOException;
 
 
 public class Main {
 
-    
+    public static class CallsJoinMapper extends Mapper<LongWritable, Text, TextPair, Text> {
+        @Override
+        protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+            ServiceCall call  = new ServiceCall(value);
+            context.write(new TextPair(call.getSystemA().toString(), "1"),
+                    new Text(call.toString()));
+        }
+    }
+
+    public static class SystemJoinMapper extends Mapper<LongWritable, Text, TextPair, Text> {
+        @Override
+        protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+            SystemInfo system = new SystemInfo(value);
+            context.write(new TextPair(system.getSystemCode().toString(), "0"), new Text(system.toString()));
+        }
+    }
 
     public static void main(String[] args) throws Exception{
         Job job = Job.getInstance();
         job.setJarByClass(JoinJob.class);
         job.setJobName("Reduce side join");
         MultipleInputs.addInputPath(job, new Path(args[0]), TextInputFormat.class, CallsJoinMapper.class);
-        MultipleInputs.addInputPath(job, new Path(args[1]), TextInputFormat.class, SystemsJoinMapper.class);
+        MultipleInputs.addInputPath(job, new Path(args[1]), TextInputFormat.class, SystemJoinMapper.class);
         FileOutputFormat.setOutputPath(job, new Path(args[2]));
         job.setPartitionerClass(TextPair.FirstaPartitioner.class);
         job.setGroupingComparatorClass(TextPair.FirstComparator.class);
