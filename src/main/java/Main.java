8fadcdb16a0a217ff2772ecc511e0d8703d62ec0
rodionovv <wasyarod@mail.ru>
2019-10-09 14:28:43 +0300

    Scripted auto-commit on change (2019-10-09 14:28:43) by gitwatch.sh

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -62,10 +62,20 @@ public class Main {
             }
         }
 
-        public static class FirstComparator extends RawComparator {
+        public static class FirstComparator extends RawComparator{
             @Override
             public int compare(byte[] b1, int s1, int l1, byte[] b2, int s2, int l2) {
                 return 0;
+            }
+
+            @Override
+            public int compare(Object o1, Object o2) {
+                return 0;
+            }
+
+            @Override
+            public boolean equals(Object obj) {
+                return false;
             }
         }
     }
