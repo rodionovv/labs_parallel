5a70fdf8157b2edae00b8ecda69e6559e882e087
rodionovv <wasyarod@mail.ru>
2019-10-09 12:19:09 +0300

    Scripted auto-commit on change (2019-10-09 12:19:09) by gitwatch.sh

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,16 @@
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.mapred.TextInputFormat;
+import org.apache.hadoop.mapred.lib.MultipleInputs;
+import org.apache.hadoop.mapreduce.Job;
+
 public class Main {
 
     public static void main(String[] args) {
-        
+        Job job = Job.getInstance();
+        job.setJarByClass(JoinJob.class);
+        job.setJobName("Reduce side join");
+        MultipleInputs.addInputPath(job, new Path(args[0]), TextInputFormat.class, CallsJoinMapper.class);
+        MultipleInputs.addInputPath(job, new Path(args[1]), TextInputFormat.class, SystemsJoinMapper.class);
     }
 
 }
