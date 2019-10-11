9e7d05fa61d2cbab1485a0857e42a37cd8055208
rodionovv <wasyarod@mail.ru>
2019-10-11 18:20:58 +0300

    add package

--- src/main/java/org/rodionov/Main.java
+++ src/main/java/org/rodionov/Main.java
@@ -1,10 +1,12 @@
+package org.rodionov;
+
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.*;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Partitioner;
 import org.apache.hadoop.mapreduce.Reducer;
-import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+
 import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
