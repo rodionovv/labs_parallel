3bee17d617f343652d21c3986d675f5e63ae29fa
rodionovv <wasyarod@mail.ru>
2019-10-12 10:57:37 +0300

    Scripted auto-commit on change (2019-10-12 10:57:36) by gitwatch.sh

--- src/main/java/org/rodionov/FirstComparator.java
+++ src/main/java/org/rodionov/FirstComparator.java
@@ -1,4 +1,17 @@
 package org.rodionov;
 
-public class FirstComparator {
+import org.apache.hadoop.io.WritableComparable;
+import org.apache.hadoop.io.WritableComparator;
+
+public static class FirstComparator extends WritableComparator {
+    public FirstComparator() {
+        super(Main.TextPair.class, true);
+    }
+
+    @Override
+    public int compare(WritableComparable o1, WritableComparable o2) {
+        Main.TextPair tp1 = (Main.TextPair) o1;
+        Main.TextPair tp2 = (Main.TextPair) o2;
+        return tp1.first.compareTo(tp2.first);
+    }
 }
