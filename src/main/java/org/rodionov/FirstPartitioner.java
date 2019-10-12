3d2f5b0076e33ff16381967a6546d6e70983dc2a
rodionovv <wasyarod@mail.ru>
2019-10-12 10:58:07 +0300

    Scripted auto-commit on change (2019-10-12 10:58:07) by gitwatch.sh

--- src/main/java/org/rodionov/FirstPartitioner.java
+++ src/main/java/org/rodionov/FirstPartitioner.java
@@ -1,4 +1,12 @@
 package org.rodionov;
 
-public class FirstPartitioner {
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Partitioner;
+
+public static class FirstPartitioner extends Partitioner<Main.TextPair, Text> {
+    @Override
+    public int getPartition(Main.TextPair textPair, Text text, int numPartitions) {
+        if (Integer.parseInt(textPair.first.toString()) < 13000) return 0;
+        else return 1 % numPartitions;
+    }
 }
