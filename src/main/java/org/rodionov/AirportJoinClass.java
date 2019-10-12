6940737e01ec9b80a812192615f6ab47b8ddadd9
rodionovv <wasyarod@mail.ru>
2019-10-12 11:00:19 +0300

    Scripted auto-commit on change (2019-10-12 11:00:19) by gitwatch.sh

--- src/main/java/org/rodionov/AirportJoinClass.java
+++ src/main/java/org/rodionov/AirportJoinClass.java
@@ -1,4 +1,20 @@
 package org.rodionov;
 
-public class AirportJoinClass {
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
+
+public static class SystemJoinMapper extends Mapper<LongWritable, Text, TextPair, Text> {
+    @Override
+    protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+
+        if (value.toString().startsWith("Code,Description")){
+            return;
+        }
+        String record = value.toString();
+        String[] parts = record.split(",", 2);
+        context.write(new TextPair(parts[0].split("\"")[1], "0"), new Text(parts[1].split("\"")[1].split("\"")[0]));
+    }
 }
