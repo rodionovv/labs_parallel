b62f992e9cb265aa43497a73dd0e86998f381fa6
rodionovv <wasyarod@mail.ru>
2019-10-12 10:59:34 +0300

    Scripted auto-commit on change (2019-10-12 10:59:34) by gitwatch.sh

--- src/main/java/org/rodionov/DelayJoinMapper.java
+++ src/main/java/org/rodionov/DelayJoinMapper.java
@@ -1,4 +1,20 @@
 package org.rodionov;
 
-public class DelayJoinMapper {
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
+
+public static class CallsJoinMapper extends Mapper<LongWritable, Text, TextPair, Text> {
+    @Override
+    protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+        if (value.toString().startsWith("\"YEAR\",\"QUARTER\"")){
+            return;
+        }
+        String record = value.toString();
+        String[] parts = record.split(",");
+        context.write(new TextPair(parts[14], "1"), new Text(parts[17]));
+
+    }
 }
