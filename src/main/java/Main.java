ba69d8c3d546ccb636955f5cc430d3cd2e543a81
rodionovv <wasyarod@mail.ru>
2019-10-28 16:26:30 +0300

    Scripted auto-commit on change (2019-10-28 16:26:30) by gitwatch.sh

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,12 @@
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaSparkContext;
 
 public class Main {
 
     public static void main(String args[]){
-        SparkConf conf = new SparkConf().
+        SparkConf conf = new SparkConf().setAppName("lab3");
+        JavaSparkContext sc = new JavaSparkContext(conf);
+        
     }
 
 }
