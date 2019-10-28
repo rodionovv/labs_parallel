efd03a1b779e0d4f134140cd9126a90f549f9cca
rodionovv <wasyarod@mail.ru>
2019-10-28 16:50:32 +0300

    Scripted auto-commit on change (2019-10-28 16:50:32) by gitwatch.sh

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,6 @@ import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import scala.Tuple2;
-
 public class Main {
 
     public static void main(String args[]){
