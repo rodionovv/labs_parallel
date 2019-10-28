e0ee5106849ee6c0cce9b8c025adc7f99af37d08
rodionovv <wasyarod@mail.ru>
2019-10-28 16:42:47 +0300

    Scripted auto-commit on change (2019-10-28 16:42:47) by gitwatch.sh

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,7 @@ import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import scala.Tuple2;
 
 public class Main {
 
@@ -10,7 +11,9 @@ public class Main {
         JavaSparkContext sc = new JavaSparkContext(conf);
         JavaRDD<String> airports = sc.textFile(args[0]);
         JavaRDD<String> delay = sc.textFile(args[1]);
-        JavaPairRDD<Long, String> = airports.mapToPair()
+        JavaPairRDD<Long, String> splitterAirports = airports.mapToPair(
+                                                        s -> new Tuple2<>(s, )
+                                                    );
     }
 
 }
