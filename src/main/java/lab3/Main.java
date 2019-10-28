4938a7c641df800ad24b4910d8c523ff510b18d9
rodionovv <wasyarod@mail.ru>
2019-10-28 23:18:51 +0300

    Scripted auto-commit on change (2019-10-28 23:18:51) by gitwatch.sh

--- src/main/java/lab3/Main.java
+++ src/main/java/lab3/Main.java
@@ -97,7 +97,7 @@ public class Main {
                                 delayCount++;
                                 if (delay > maxDelay) maxDelay = delay;
                             }
-                        }
+
 
                     }
                 }
