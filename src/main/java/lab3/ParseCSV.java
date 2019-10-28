6435eab564163df2974a49bd14f94d9466dc7791
rodionovv <wasyarod@mail.ru>
2019-10-28 23:50:27 +0300

    Scripted auto-commit on change (2019-10-28 23:50:27) by gitwatch.sh

--- src/main/java/lab3/ParseCSV.java
+++ src/main/java/lab3/ParseCSV.java
@@ -32,7 +32,7 @@ public class ParseCSV {
     }
 
     static JavaRDD<String> readCSV(JavaSparkContext sc, String path, String header) {
-        JavaRDD<String> data = sc.textFile(path).;
+        JavaRDD<String> data = sc.textFile(path)
     }
 
 }
