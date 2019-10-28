b244e4721d541366bcd95dd2941f5ef27dd139fd
rodionovv <wasyarod@mail.ru>
2019-10-28 22:52:59 +0300

    Scripted auto-commit on change (2019-10-28 22:52:59) by gitwatch.sh

--- src/main/java/lab3/Main.java
+++ src/main/java/lab3/Main.java
@@ -82,7 +82,7 @@ public class Main {
         data.groupByKey();
         data.mapValues(
                 s -> {
-                              
+                    
                 }
         );
     }
