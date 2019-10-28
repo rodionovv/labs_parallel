2c119b897c2f4f10529eeeb562b850e95245d006
rodionovv <wasyarod@mail.ru>
2019-10-28 16:31:55 +0300

    Scripted auto-commit on change (2019-10-28 16:31:55) by gitwatch.sh

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,6 +9,7 @@ public class Main {
         JavaSparkContext sc = new JavaSparkContext(conf);
         JavaRDD<String> airports = sc.textFile(args[0]);
         JavaRDD<String> delay = sc.textFile(args[1]);
+        delay.mato
     }
 
 }
