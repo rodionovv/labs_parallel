a8d3f2f4a10eca9489a712ad9ad0bb01dc5f3ed9
rodionovv <wasyarod@mail.ru>
2019-10-28 16:41:02 +0300

    Scripted auto-commit on change (2019-10-28 16:41:02) by gitwatch.sh

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,5 @@
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 
@@ -9,6 +10,7 @@ public class Main {
         JavaSparkContext sc = new JavaSparkContext(conf);
         JavaRDD<String> airports = sc.textFile(args[0]);
         JavaRDD<String> delay = sc.textFile(args[1]);
+        JavaPairRDD<Long, String> = airports.
     }
 
 }
