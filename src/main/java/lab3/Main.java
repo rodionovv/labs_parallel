1b407829ffd509f0297556e954c379d53df64da8
rodionovv <wasyarod@mail.ru>
2019-10-28 21:13:59 +0300

    Scripted auto-commit on change (2019-10-28 21:13:59) by gitwatch.sh

--- src/main/java/lab3/Main.java
+++ src/main/java/lab3/Main.java
@@ -4,11 +4,18 @@ import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import scala.Serializable;
 import scala.Tuple2;
 
 public class Main {
 
-    
+    public class DataStorage implements Serializable {
+        private Tuple2<String, String> key;
+        private String 
+        DataStorage(){
+
+        }
+    }
 
 
     public static void main(String args[]){
