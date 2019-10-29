5099662c889c0ebfd11099397dc14a41aa3adc28
rodionovv <wasyarod@mail.ru>
2019-10-29 10:14:24 +0300

    Scripted auto-commit on change (2019-10-29 10:14:24) by gitwatch.sh

--- src/main/java/lab3/AirportPair.java
+++ src/main/java/lab3/AirportPair.java
@@ -1,4 +1,48 @@
 package lab3;
 
+import java.util.Objects;
+
 public class AirportPair {
+
+
+    private String originAirport;
+    private String destAirport;
+    AirportPair(String originAirport, String destAirport) {
+        this.originAirport = originAirport;
+        this.destAirport = destAirport;
+    }
+
+    public String getDestAirport() {
+        return this.destAirport;
+    }
+
+    public String getOriginAirport() {
+        return this.originAirport;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        Main.AirportPair p = (Main.AirportPair) obj;
+        if (this.originAirport.equals(p.originAirport)){
+            if (this.destAirport.equals(p.destAirport)){
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(originAirport, destAirport);
+    }
+
+    @Override
+    public String toString() {
+        return "Flight from " + originAirport + " to " + destAirport;
+    }
+}
+
 }
