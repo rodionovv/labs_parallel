0a12804ad7249bcc50d2f8bb70e608bdfac6e18a
rodionovv <wasyarod@mail.ru>
2019-10-30 15:09:01 +0300

    Scripted auto-commit on change (2019-10-30 15:09:01) by gitwatch.sh

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,18 +7,20 @@ import akka.http.javadsl.model.HttpRequest;
 import akka.http.javadsl.model.HttpResponse;
 import akka.http.javadsl.server.AllDirectives;
 import akka.http.javadsl.server.Complete;
+import akka.http.javadsl.server.Route;
 import akka.stream.ActorMaterializer;
 import akka.stream.javadsl.Flow;
 
+import java.io.IOException;
 import java.util.concurrent.CompletionStage;
 
-class Main {
+import static akka.http.javadsl.server.Directives.*;
+
+class Main extends AllDirectives {
 
 
 
-    public static void  main(String[] args) extends AllDirectives
-
-    {
+    public static void  main(String[] args)  throws IOException {
         ActorSystem system = ActorSystem.create("routes");
         final Http http =  Http.get(system);
         final ActorMaterializer materializer = ActorMaterializer.create(system);
@@ -26,7 +28,17 @@ class Main {
         final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = app.createRoute().flow(system,materializer);
         final CompletionStage<ServerBinding> binding = http.bindAndHandle(routeFlow,
                 ConnectHttp.toHost("localhost", 8080), materializer);
-        System.out.println();
+        System.out.println("Server online at http://localhost:8080/\nPress RETURN to stop...");
+        System.in.read();
+        binding
+                .thenCompose(ServerBinding::unbind)
+                .thenAccept(unbound -> system.terminate());
+    }
 
+    private Route createRoute() {
+        return concat(
+                path("hello", () ->
+                        get(() ->
+                            complete("<h1>Say hello to akka-http</h1>"))));
     }
 }
