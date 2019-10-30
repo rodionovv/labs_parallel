1fa747fc58cb853c6bdf09ecc6cfe2399e77d208
rodionovv <wasyarod@mail.ru>
2019-10-30 15:05:05 +0300

    Scripted auto-commit on change (2019-10-30 15:05:05) by gitwatch.sh

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,16 @@
+import akka.NotUsed;
 import akka.actor.ActorSystem;
+import akka.http.javadsl.ConnectHttp;
 import akka.http.javadsl.Http;
+import akka.http.javadsl.ServerBinding;
+import akka.http.javadsl.model.HttpRequest;
+import akka.http.javadsl.model.HttpResponse;
 import akka.http.javadsl.server.AllDirectives;
+import akka.http.javadsl.server.Complete;
 import akka.stream.ActorMaterializer;
+import akka.stream.javadsl.Flow;
+
+import java.util.concurrent.CompletionStage;
 
 class Main {
 
@@ -13,6 +22,11 @@ class Main {
         ActorSystem system = ActorSystem.create("routes");
         final Http http =  Http.get(system);
         final ActorMaterializer materializer = ActorMaterializer.create(system);
-  
+        Main app = new Main();
+        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = app.createRoute().flow(system,materializer);
+        final CompletionStage<ServerBinding> binding = http.bindAndHandle(routeFlow,
+                ConnectHttp.toHost("localhost", 8080), materializer);
+        System.out.println();
+
     }
 }
