92d886f7afa30d2a3e869455c21c5c2e7bdb7242
rodionovv <wasyarod@mail.ru>
2019-10-30 15:01:39 +0300

    Scripted auto-commit on change (2019-10-30 15:01:39) by gitwatch.sh

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,16 +1,18 @@
 import akka.actor.ActorSystem;
 import akka.http.javadsl.Http;
+import akka.http.javadsl.server.AllDirectives;
 import akka.stream.ActorMaterializer;
 
 class Main {
 
 
 
-    public static void  main(String[] args) {
+    public static void  main(String[] args) extends AllDirectives
+
+    {
         ActorSystem system = ActorSystem.create("routes");
         final Http http =  Http.get(system);
         final ActorMaterializer materializer = ActorMaterializer.create(system);
-        MainHttp instance = new MainHttp(system);
-
+  
     }
 }
