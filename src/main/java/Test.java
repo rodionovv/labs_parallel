c6d0d9a2323baed7f19a55cb9aa32240c3f5cbf6
rodionovv <wasyarod@mail.ru>
2019-11-13 12:50:41 +0300

    Scripted auto-commit on change (2019-11-13 12:50:41) by gitwatch.sh

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -15,10 +15,32 @@ public class Test {
     @JsonCreator
     public Test(@JsonProperty("testName") String testName,
                 @JsonProperty("expectedResult") String expectedResult,
-                @JsonProperty("params") String params) {
-
+                @JsonProperty("params") Object[] params) {
         this.testName = testName;
         this.expectedResult = expectedResult;
         this.params = params;
+        this.realResult = "NONE";
+        this.checker = "NOT READY YET";
+    }
+
+
+    public Object[] getParams() {
+        return params;
+    }
+
+    public String getChecker() {
+        return checker;
+    }
+
+    public String getRealResult() {
+        return realResult;
+    }
+
+    public String getExpectedResult() {
+        return expectedResult;
+    }
+
+    public String getTestName() {
+        return testName;
     }
 }
