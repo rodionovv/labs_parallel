94be0b3258d02dfca09106ce5ef239a9be1e5ffc
rodionovv <wasyarod@mail.ru>
2019-11-13 12:56:10 +0300

    Scripted auto-commit on change (2019-11-13 12:56:09) by gitwatch.sh

--- src/main/java/MainActor.java
+++ src/main/java/MainActor.java
@@ -1,2 +1,10 @@
+import akka.actor.ActorRef;
+
 public class MainActor {
+    private final static int NUM_ROUNDS = 5;
+    private final ActorRef executor;
+    private final ActtorRef storage;
+
+
+
 }
