b8b7692a6d439f5c08e5f768fde0c0d02237a993
rodionovv <wasyarod@mail.ru>
2019-11-13 12:49:10 +0300

    Scripted auto-commit on change (2019-11-13 12:49:10) by gitwatch.sh

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,4 +1,24 @@
-package PACKAGE_NAME;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class Test {
+
+    private final String testName;
+    private final String expectedResult;
+    private final String realResult;
+    private final String checker;
+    private  final Object[] params;
+
+
+
+
+    @JsonCreator
+    public Test(@JsonProperty("testName") String testName,
+                @JsonProperty("expectedResult") String expectedResult,
+                @JsonProperty("params") String params) {
+
+        this.testName = testName;
+        this.expectedResult = expectedResult;
+        this.params = params;
+    }
 }
