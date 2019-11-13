e4e79c39b8660b66f2c1b45ee6698bed92a9fdce
rodionovv <wasyarod@mail.ru>
2019-11-13 13:15:32 +0300

    Scripted auto-commit on change (2019-11-13 13:15:32) by gitwatch.sh

--- src/main/java/JSExecutorActor.java
+++ src/main/java/JSExecutorActor.java
@@ -30,7 +30,9 @@ public class JSExecutorActor extends AbstractActor {
                 check = "correct";
             }
 
-            StorageMessage storageMessage = new StorageMessage
+            StorageMessage storageMessage = new StorageMessage{
+                
+            }
         })
     }
 }
