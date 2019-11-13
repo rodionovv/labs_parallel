09e5c1d59bb41a7a6bcd563ee0548e3d4b2740d7
rodionovv <wasyarod@mail.ru>
2019-11-13 13:24:37 +0300

    Scripted auto-commit on change (2019-11-13 13:24:37) by gitwatch.sh

--- src/main/java/StorageActor.java
+++ src/main/java/StorageActor.java
@@ -1,6 +1,15 @@
 import akka.actor.AbstractActor;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class StorageActor extends AbstractActor {
 
-    
+    private HashMap<Integer, ArrayList<StorageMessage>> data = new HashMap<>();
+
+
+    @Override
+    public Receive createReceive() {
+        return null;
+    }
 }
