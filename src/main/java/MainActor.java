325a2f87fba11fe8b890ac15224c314c953da5e1
rodionovv <wasyarod@mail.ru>
2019-11-13 12:58:06 +0300

    Scripted auto-commit on change (2019-11-13 12:58:06) by gitwatch.sh

--- src/main/java/MainActor.java
+++ src/main/java/MainActor.java
@@ -1,10 +1,16 @@
+import akka.actor.AbstractActor;
 import akka.actor.ActorRef;
 
-public class MainActor {
+public class MainActor extends AbstractActor {
     private final static int NUM_ROUNDS = 5;
     private final ActorRef executor;
     private final ActorRef storage;
 
+
+    @Override
+    public Receive createReceive() {
+        return null;
+    }
 
     public MainActor() {
         this.executor = getContext().a;
