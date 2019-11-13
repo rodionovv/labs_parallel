d7abb0d7851e1768d500eb1ba0de7aa1e23c2efa
rodionovv <wasyarod@mail.ru>
2019-11-13 13:27:00 +0300

    Scripted auto-commit on change (2019-11-13 13:27:00) by gitwatch.sh

--- src/main/java/StorageCommand.java
+++ src/main/java/StorageCommand.java
@@ -1,16 +1,16 @@
 public class StorageCommand {
 
-    private final int storageID;
+    private final int packageID;
     private final StorageMessage storageMessage;
 
 
-    public StorageCommand(int storageID, StorageMessage storageMessage) {
-        this.storageID = storageID;
+    public StorageCommand(int packageID, StorageMessage storageMessage) {
+        this.packageID = pa;
         this.storageMessage = storageMessage;
     }
 
-    public int getStorageID() {
-        return storageID;
+    public int getPackageID() {
+        return packageID;
     }
 
     public StorageMessage getStorageMessage() {
