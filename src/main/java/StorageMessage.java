0142cbfb3c7557f14b8088a5d4f0fb1d3d8e1297
rodionovv <wasyarod@mail.ru>
2019-11-13 13:19:58 +0300

    Scripted auto-commit on change (2019-11-13 13:19:58) by gitwatch.sh

--- src/main/java/StorageMessage.java
+++ src/main/java/StorageMessage.java
@@ -1,6 +1,23 @@
 public class StorageMessage {
 
-    private final String r
+    private final String result;
+    private final String expectedResult;
+    private final String testName;
+    private final String checker;
+    private final Object[] param;
+
+    private StorageMessage(String result,
+                           String expectedResult,
+                           String checker,
+                           Object[] param,
+                           String testName) {
+        this.result = result;
+        this.expectedResult = expectedResult;
+        this.checker = checker;
+        this.param = param;
+        this.testName = testName;
+        th
+    }
 
 
 }
