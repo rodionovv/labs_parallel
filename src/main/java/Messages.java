e81fec67f2ce7b84ca2856ee928c88d6eb55a1d4
rodionovv <wasyarod@mail.ru>
2019-11-13 12:44:02 +0300

    Scripted auto-commit on change (2019-11-13 12:44:02) by gitwatch.sh

--- src/main/java/Messages.java
+++ src/main/java/Messages.java
@@ -3,4 +3,11 @@ public class Messages {
     private int packageId;
 
 
+    public Messages(int packageId) {
+        this.packageId = packageId;
+    }
+
+    public int getPackageId() {
+        return packageId;
+    }
 }
