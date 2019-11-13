7ac950ac4d4fa4e7a4bd6c47e6fd4fb1df410209
rodionovv <wasyarod@mail.ru>
2019-11-13 13:11:09 +0300

    Scripted auto-commit on change (2019-11-13 13:11:09) by gitwatch.sh

--- src/main/java/JSExecutorActor.java
+++ src/main/java/JSExecutorActor.java
@@ -1,10 +1,24 @@
 import akka.actor.AbstractActor;
+import akka.japi.pf.ReceiveBuilder;
+import javafx.util.Pair;
+
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
 
 public class JSExecutorActor extends AbstractActor {
 
 
     @Override
     public Receive createReceive() {
-        return null;
+        return ReceiveBuilder.create().match(ExcuteMessage.class, message -> {
+            Pair<Integer, Functions> msg = message.getMsg();
+            int index = msg.getKey();
+            Functions functions = msg.getValue();
+            Test test = functions.getTests()[index];
+            ScriptEngine engine = new ScriptEngineManager().getEngineByName("nashorn");
+            try {
+                
+            }
+        })
     }
 }
