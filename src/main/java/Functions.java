03bd558c768c02c22d4f4608464649475ec425b8
rodionovv <wasyarod@mail.ru>
2019-11-13 12:39:55 +0300

    Scripted auto-commit on change (2019-11-13 12:39:55) by gitwatch.sh

--- src/main/java/Functions.java
+++ src/main/java/Functions.java
@@ -1,2 +1,23 @@
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class Functions {
+    private final int packageID;
+    private final String script;
+    private final String functionName;
+    private final Test[] tests;
+
+    @JsonCreator
+    public Functions(
+            @JsonProperty("packageID") String packageID,
+            @JsonProperty("jsScript") String script,
+            @JsonProperty("functionName") String functionName,
+            @JsonProperty("tests") Test[] tests) {
+        this.packageID = Integer.parseInt(packageID);
+        this.functionName = functionName;
+        this.tests = tests;
+        this.script = script;
+    }
+
+    
 }
