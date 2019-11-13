ed05615493fd93feec494e65d3bbc68796280a24
rodionovv <wasyarod@mail.ru>
2019-11-13 13:12:15 +0300

    Scripted auto-commit on change (2019-11-13 13:12:15) by gitwatch.sh

--- src/main/java/JSExecutorActor.java
+++ src/main/java/JSExecutorActor.java
@@ -24,7 +24,7 @@ public class JSExecutorActor extends AbstractActor {
                 e.printStackTrace();
             }
             Invocable invocable = (Invocable) engine;
-            String e
+            String res
         })
     }
 }
