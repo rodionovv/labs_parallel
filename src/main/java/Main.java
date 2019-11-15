0b565ce0f288419956ce91642c8b7116bbcc839e
rodionovv <wasyarod@mail.ru>
2019-11-15 19:58:39 +0300

    Scripted auto-commit on change (2019-11-15 19:58:39) by gitwatch.sh

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -116,7 +116,7 @@ class Main extends AllDirectives {
         String result = "";
         HttpPost post = new HttpPost("http://localhost:8080/");
         JSONParser jsonParser = new JSONParser();
-        try(FileReader reader = new FileReader("../tests.json")){
+        try(FileReader reader = new FileReader("tests.json")){
             System.out.println("here");
             Object obj = jsonParser.parse(reader);
             JSONArray list = (JSONArray) obj;
