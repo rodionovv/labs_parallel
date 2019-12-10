ba1b0293d63d9c2123b18c5489b0e61b0ee2d9d5
rodionovv <wasyarod@mail.ru>
2019-12-10 18:40:42 +0300

    Scripted auto-commit on change (2019-12-10 18:40:42) by gitwatch.sh

--- src/main/java/lab6/Zoo.java
+++ src/main/java/lab6/Zoo.java
@@ -64,6 +64,8 @@ public class Zoo {
                 try {
                     servers.add(new String(zoo.getData(ZOO_KEEPER_CHILD_DIR + s, false, null)));
                 } catch (KeeperException | InterruptedException e) {
+
+                    System.out.println("in mistake");
                     e.printStackTrace();
                 }
             }
