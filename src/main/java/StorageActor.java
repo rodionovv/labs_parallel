ad447cd3e91846dd3f53a3d1d1ac33558b6fbbad
rodionovv <wasyarod@mail.ru>
2019-12-10 16:54:34 +0300

    Scripted auto-commit on change (2019-12-10 16:54:34) by gitwatch.sh

--- src/main/java/StorageActor.java
+++ src/main/java/StorageActor.java
@@ -1,21 +1,29 @@
 import akka.actor.AbstractActor;
 import akka.japi.pf.ReceiveBuilder;
 import java.util.List;
+import java.util.Random;
+
 public class StorageActor extends AbstractActor {
 
     List<String> serversData;
+    int len;
 
     @Override
     public Receive createReceive() {
         return ReceiveBuilder.create().match(
                 PortMessage.class,
                 msg -> {
-                    
+                    int randomPort;
+                    Random rand = new Random()
+                    while(serversData.get(new Random().nextInt(len)).equals(msg.getPort())) {
+                        randomPort =
+                    }
                 }
         ).match(
                 ServerMessage.class,
                 msg -> {
                     serversData = msg.getServersData();
+                    len = serversData.size();
                 }
         ).build();
     }
