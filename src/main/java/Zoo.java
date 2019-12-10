627e3aa26e429ac7ab59065e6f4d216fadc0634f
rodionovv <wasyarod@mail.ru>
2019-12-10 13:36:31 +0300

    Scripted auto-commit on change (2019-12-10 13:36:31) by gitwatch.sh

--- src/main/java/Zoo.java
+++ src/main/java/Zoo.java
@@ -1,9 +1,30 @@
+import org.apache.zookeeper.ZooDefs;
+import org.apache.zookeeper.ZooKeeper;
+
 public class Zoo {
 
-    private int port;
+    private static final String ZOO_KEEPER_HOST = "127.0.0.1:2181";
+    private static final String ZOO_KEEPER_SERVER_DIR = "/servers";
+    private static final String ZOO_KEEPER_CHILD_DIR = "/servers/";
+    private final static int TIMEOUT = 5000;
+
+    private String port;
+    private ZooKeeper zoo;
 
     Zoo(int port) {
-        this.port = port;
+        this.port = Integer.toString(port);
+        this.zoo = new ZooKeeper(
+                ZOO_KEEPER_HOST,
+                TIMEOUT,
+                //watcher
+        );
     }
-    
+
+    public void create() {
+        ZOO_KEEPER_CHILD_DIR + port,
+        port.getBytes(),
+        ZooDefs.Ids.OPEN_ACL_UNSAFE,
+        
+    }
+
 }
