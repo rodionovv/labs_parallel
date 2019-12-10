6a0dceb64451af205cf60bee844bd5a82c0c96ac
rodionovv <wasyarod@mail.ru>
2019-12-10 13:46:47 +0300

    Scripted auto-commit on change (2019-12-10 13:46:47) by gitwatch.sh

--- src/main/java/UpdateWatcher.java
+++ src/main/java/UpdateWatcher.java
@@ -1,10 +1,17 @@
 import org.apache.zookeeper.WatchedEvent;
 import org.apache.zookeeper.Watcher;
+import org.apache.zookeeper.ZooKeeper;
 
 public class UpdateWatcher implements Watcher {
 
+    private ZooKeeper zoo;
+
+    UpdateWatcher(ZooKeeper zoo) {
+        this.zoo = zoo;
+    }
+
     @Override
     public void process(WatchedEvent watchedEvent) {
-
+        
     }
 }
