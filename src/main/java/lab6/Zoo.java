c33c8981af347a3f072f7e42d047d633aa6d7ff6
rodionovv <wasyarod@mail.ru>
2019-12-10 17:07:29 +0300

    Scripted auto-commit on change (2019-12-10 17:07:29) by gitwatch.sh

--- src/main/java/lab6/Zoo.java
+++ src/main/java/lab6/Zoo.java
@@ -31,7 +31,7 @@ public class Zoo {
     public void create() throws KeeperException, InterruptedException {
         this.zoo.create(
                 ZOO_KEEPER_CHILD_DIR + port,
-                port.getBytes(),
+                Integer.toString(port).getBytes(),
                 ZooDefs.Ids.OPEN_ACL_UNSAFE,
                 CreateMode.EPHEMERAL
         );
