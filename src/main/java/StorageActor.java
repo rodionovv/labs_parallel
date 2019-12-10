39cb267b127774e22cd26f64644a876f83d34666
rodionovv <wasyarod@mail.ru>
2019-12-10 16:38:08 +0300

    Scripted auto-commit on change (2019-12-10 16:38:08) by gitwatch.sh

--- src/main/java/StorageActor.java
+++ src/main/java/StorageActor.java
@@ -1,8 +1,21 @@
 import akka.actor.AbstractActor;
+import akka.japi.pf.ReceiveBuilder;
 
 public class StorageActor extends AbstractActor {
+
+
     @Override
     public Receive createReceive() {
-        return null;
+        return ReceiveBuilder.create().match(
+                //portClass,
+                msg -> {
+
+                }
+        ).match(
+                //messagefromserver.class,
+                msg -> {
+
+                }
+        ).build();
     }
 }
