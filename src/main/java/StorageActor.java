a9ba4540d8eab4c3d0ce5876ec233a99232d3519
rodionovv <wasyarod@mail.ru>
2019-12-10 16:45:51 +0300

    Scripted auto-commit on change (2019-12-10 16:45:51) by gitwatch.sh

--- src/main/java/StorageActor.java
+++ src/main/java/StorageActor.java
@@ -12,7 +12,7 @@ public class StorageActor extends AbstractActor {
 
                 }
         ).match(
-                ServerMessage.class
+                ServerMessage.class,
                 msg -> {
 
                 }
