696cb8c63a4ccfe43c3eb5c0f0acf008ff7554ee
rodionovv <wasyarod@mail.ru>
2019-12-12 16:51:37 +0300

    Scripted auto-commit on change (2019-12-12 16:51:37) by gitwatch.sh

--- src/main/java/CacheStorage.java
+++ src/main/java/CacheStorage.java
@@ -1,5 +1,31 @@
+import org.zeromq.*;
+
 public class CacheStorage {
 
-    
+    left = Integer.parseInt(args[1]);
+    right = Integer.parseInt(args[2]);
+    str = args[0].substring(left, right);
+        try (ZContext ctx = new ZContext()) {
+        ZMQ.Socket worker = ctx.createSocket(SocketType.DEALER);
+        worker.setHWM(0);
+        worker.setIdentity("W".getBytes(ZMQ.CHARSET));
+        worker.connect("tcp://localhost:5556");
+        //ZMQ.Poller poller = ctx.createPoller(1);
+        //poller.register(worker, ZMQ.Poller.POLLIN);
+        long start = System.currentTimeMillis();
+        while (!Thread.currentThread().isInterrupted()) {
+            if (System.currentTimeMillis() - start > 5000) {
+                ZMsg msg = new ZMsg();
+                msg.addString(left + "-" + right);
+                msg.send(worker);
+                start = System.currentTimeMillis();
+            }
+            ZMsg msg = ZMsg.recvMsg(worker);
+            ZFrame content = msg.getLast();
+            String s = content.toString();
+            System.out.println(s);
+            msg.send(worker);
+        }
+    }
 
 }
