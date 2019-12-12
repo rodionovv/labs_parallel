a17328449f2571d72fa271323d1842b20a35548a
rodionovv <wasyarod@mail.ru>
2019-12-12 21:16:44 +0300

    Scripted auto-commit on change (2019-12-12 21:16:44) by gitwatch.sh

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -21,7 +21,6 @@ public class Client {
                 }
                 ZMsg message = new ZMsg();
                 for (int i = 0; i < command.length; i++) {
-                    System.out.println(command[i]);
                     message.add(command[i]);
                 }
                 message.send(req);
