f9731b56ec199838a602de8a147688abb28f7402
rodionovv <wasyarod@mail.ru>
2019-12-12 16:16:36 +0300

    Scripted auto-commit on change (2019-12-12 16:16:36) by gitwatch.sh

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,15 @@
+import org.zeromq.ZContext;
+import org.zeromq.ZMQ;
 
 class Main{
 
 
 
-    public static void  main(String[] args){
-        
+    public static void  main(String[] args) {
+
+        try (ZContext context = new ZContext()) {
+            ZMQ.Socket frontend = context.createSocket(ZMQ.ROUTER);
+        }
+
     }
 }
