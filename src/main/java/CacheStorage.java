5fe0352862f12a95a445f605b6167a5a557f950e
rodionovv <wasyarod@mail.ru>
2019-12-12 17:59:54 +0300

    Scripted auto-commit on change (2019-12-12 17:59:54) by gitwatch.sh

--- src/main/java/CacheStorage.java
+++ src/main/java/CacheStorage.java
@@ -10,27 +10,26 @@ public class CacheStorage {
         right = args[2];
         str = args[0].substring(Integer.parseInt(left), Integer.parseInt(right));
         try (ZContext ctx = new ZContext()) {
-            ZMQ.Socket worker = ctx.createSocket(SocketType.DEALER);
-            worker.connect("tcp://localhost:5560");
+            ZMQ.Socket dealer = ctx.createSocket(SocketType.DEALER);
+            dealer.connect("tcp://localhost:5560");
             long start = System.currentTimeMillis();
 
             ZMQ.Poller poller = ctx.createPoller(2);
-            poller.register(worker, ZMQ.Poller.POLLIN);
+            poller.register(dealer, ZMQ.Poller.POLLIN);
 
             while (!Thread.currentThread().isInterrupted()) {
                 if (System.currentTimeMillis() - start > 5000) {
                     ZMsg messageSend = new ZMsg();
-                    System.out.println("5 secs later");
                     messageSend.addString(left + "-" + right);
-                    messageSend.send(worker);
+                    messageSend.send(dealer);
                     start = System.currentTimeMillis();
                 }
                 if (poller.pollin(0)) {
-                    ZMsg messageRecieved = ZMsg.recvMsg(worker);
+                    ZMsg messageRecieved = ZMsg.recvMsg(dealer);
                     ZFrame content = messageRecieved.getLast();
                     String s = content.toString();
                     System.out.println(s);
-                    messageRecieved.send(worker);
+                    messageRecieved.send(dealer);
                 }
             }
         }
--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -1,9 +1,16 @@
+import org.zeromq.SocketType;
 import org.zeromq.ZContext;
+import org.zeromq.ZMQ;
+import org.zeromq.ZMsg;
 
 public class Client {
 
+    private static final String FRONTEND_ADDRESS = "tcp://*:5559";
+
     public static void main(String[] args) {
         try (ZContext context = new ZContext()) {
+            ZMQ.Socket req = context.createSocket(SocketType.REQ);
+            req.connect("tcp://*:5559");
             
         }
     }
