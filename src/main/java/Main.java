31021a2c276413b86811fd0313b7cd36328d20b1
rodionovv <wasyarod@mail.ru>
2019-12-12 18:35:45 +0300

    Scripted auto-commit on change (2019-12-12 18:35:45) by gitwatch.sh

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,6 +5,7 @@ import org.zeromq.ZMQ.Socket;
 
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.Map;
 
 class Main{
 
@@ -38,7 +39,9 @@ class Main{
                         for (ZFrame f : message) {
                             if (f.toString().equals("Get")) {
                                 int index = Integer.parseInt(message.getLast().toString());
-                                
+                                for (Map.Entry<ZFrame, Pair<Integer, Integer>> entry : hashStorage.entrySet()) {
+                                    
+                                }
                                 messageSend.add(message.getLast());
                             }
                             if (f.toString().equals("Set")) {
