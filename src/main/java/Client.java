f9dbfd7fd89c84ae2ff2e3efcb57ecc7c317b445
rodionovv <wasyarod@mail.ru>
2019-12-12 17:57:31 +0300

    Scripted auto-commit on change (2019-12-12 17:57:31) by gitwatch.sh

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -1,3 +1,10 @@
+import org.zeromq.ZContext;
+
 public class Client {
-    
+
+    public static void main(String[] args) {
+        try (ZContext context = new ZContext()) {
+            
+        }
+    }
 }
