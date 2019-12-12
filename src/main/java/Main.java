1c7c9339c0fb08ec0656964637f3877e22b47a7c
rodionovv <wasyarod@mail.ru>
2019-12-12 16:25:13 +0300

    Scripted auto-commit on change (2019-12-12 16:25:13) by gitwatch.sh

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,32 +1,29 @@
 import org.zeromq.ZContext;
 import org.zeromq.*;
 import org.zeromq.ZMQ.Socket;
-import zmq.poll.Poller;
 
 import java.io.IOException;
 
 class Main{
 
-
+    private static final String FRONTEND_ADDRESS = "tcp://*:5559";
+    private static final String BACKEND_ADDRESS = "tcp://*:5560";
 
     public static void  main(String[] args) throws IOException {
-
-        private static final String FRONTEND_ADRES =
-        
 
         try (ZContext context = new ZContext()) {
 
             Socket frontend = context.createSocket(SocketType.ROUTER);
-            frontend.bind("tcp://*:5559");
+            frontend.bind(FRONTEND_ADDRESS);
 
             Socket backend = context.createSocket(SocketType.ROUTER);
-            backend.bind("tcp://*:5560");
+            backend.bind(BACKEND_ADDRESS);
 
             ZMQ.Poller items = context.createPoller(2);
             items.register(frontend, ZMQ.Poller.POLLIN);
             items.register(backend, ZMQ.Poller.POLLIN);
 
-
+            
         }
 
     }
