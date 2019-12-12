5678378c7833dcde2cf4efd68dd92282472c3077
rodionovv <wasyarod@mail.ru>
2019-12-12 16:17:58 +0300

    Scripted auto-commit on change (2019-12-12 16:17:58) by gitwatch.sh

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,18 @@
 import org.zeromq.ZContext;
 import org.zeromq.ZMQ;
+import org.zeromq.ZMQ.Socket;
+
+import java.io.IOException;
 
 class Main{
 
 
 
-    public static void  main(String[] args) {
+    public static void  main(String[] args) throws IOException {
 
         try (ZContext context = new ZContext()) {
             ZMQ.Socket frontend = context.createSocket(ZMQ.ROUTER);
+            ZMQ.Socket backend = context.createSocket(ZMQ.ROUTER);
         }
 
     }
