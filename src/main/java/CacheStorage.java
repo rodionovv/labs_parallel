d5838e07c7f314635763374bdabb9107755fd864
rodionovv <wasyarod@mail.ru>
2019-12-14 10:59:14 +0300

    Scripted auto-commit on change (2019-12-14 10:59:14) by gitwatch.sh

--- src/main/java/CacheStorage.java
+++ src/main/java/CacheStorage.java
@@ -17,6 +17,7 @@ public class CacheStorage {
         right = Integer.parseInt(args[2]);
         str = new StringBuilder(args[0].substring(left, right));
         try (ZContext context = new ZContext()) {
+
             ZMQ.Socket dealer = context.createSocket(SocketType.DEALER);
             dealer.connect(BACKEND_ADDRESS);
             long start = System.currentTimeMillis();
@@ -38,6 +39,8 @@ public class CacheStorage {
                     start = System.currentTimeMillis();
                 }
                 if (poller.pollin(0)) {
+
+
                     ZMsg messageReceive = ZMsg.recvMsg(dealer);
                     if (messageReceive.size() == 2) {
                         ZMsg responseMessage = new ZMsg();
@@ -48,6 +51,8 @@ public class CacheStorage {
                         responseMessage.add("" + str.charAt(index - left));
                         responseMessage.send(dealer);
                     }
+
+
                     if (messageReceive.size() == 3) {
                         ZMsg responseMessage = new ZMsg();
                         String value = messageReceive.pollLast().toString();
