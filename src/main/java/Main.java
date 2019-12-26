fd11ad42a0e95d0fa6697c366da47a818e5d6a38
rodionovv <wasyarod@mail.ru>
2019-12-26 18:09:33 +0300

    Scripted auto-commit on change (2019-12-26 18:09:33) by gitwatch.sh

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -115,7 +115,7 @@ class Main extends AllDirectives {
                                                                                   .toMat(fold, Keep.right()), Keep.left()).run(materializer);
                                                       }).thenCompose(sum -> {
                                                           Patterns.ask(maiActor, new msg);
-                                                          double 
+                                                          double midVal = (double) sum;
                                                       });
                                     });
                         }
