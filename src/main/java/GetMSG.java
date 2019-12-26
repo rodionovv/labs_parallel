174f1f37355bf1b7f585654194c30ce3bbe8ecba
rodionovv <wasyarod@mail.ru>
2019-12-26 18:24:02 +0300

    Scripted auto-commit on change (2019-12-26 18:24:02) by gitwatch.sh

--- src/main/java/GetMSG.java
+++ src/main/java/GetMSG.java
@@ -6,4 +6,17 @@ public class GetMSG {
     Pair<String, Integer> msgPair;
 
 
+    public Pair<String, Integer> getMsgPair() {
+        return msgPair;
+    }
+
+    public int getCount() {
+       return msgPair.getValue();
+    }
+
+    public String getValue() {
+        return msgPair.getKey();
+    }
+
+
 }
