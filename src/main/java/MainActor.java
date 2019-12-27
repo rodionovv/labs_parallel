e4d51334cadf2868a7205d286fa7823186c10569
rodionovv <wasyarod@mail.ru>
2019-12-27 15:54:53 +0300

    Scripted auto-commit on change (2019-12-27 15:54:53) by gitwatch.sh

--- src/main/java/MainActor.java
+++ src/main/java/MainActor.java
@@ -1,2 +1,9 @@
-public class MainActor extends M {
+import akka.actor.AbstractActor;
+
+public class MainActor extends AbstractActor {
+
+    @Override
+    public Receive createReceive() {
+        return null;
+    }
 }
